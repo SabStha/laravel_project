860d2b782265f4b833d03009854add680ead52b6
pldannam <0o0.dankhai1996@gmail.com>
2025-01-29 17:01:32 +0900

    dbupdate

---
+++ database/migrations/2025_01_29_071132_create_employers_table.php
@@ -0,0 +1,41 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employers', function (Blueprint $table) {
+     
+            if (!Schema::hasColumn('employers', 'id')) {
+                $table->id(); 
+            }
+
+            if (!Schema::hasColumn('employers', 'user_id')) {
+                $table->integer('user_id')->unsigned(); 
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employers', function (Blueprint $table) {
+
+            if (Schema::hasColumn('employers', 'user_id')) {
+                $table->dropColumn('user_id');
+            }
+
+            if (Schema::hasColumn('employers', 'id')) {
+                $table->dropColumn('id');
+            }
+        });
+    }
+};
