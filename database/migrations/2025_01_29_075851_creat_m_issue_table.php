aa9c8a3b7d004da72a90fa2a26f215c2c8b53c1d
pldannam <0o0.dankhai1996@gmail.com>
2025-02-03 16:45:32 +0900

    DB Finish

--- database/migrations/2025_01_29_075851_creat_m_issue_table.php
+++ database/migrations/2025_01_29_075851_creat_m_issue_table.php
@@ -9,18 +9,21 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('issues', function (Blueprint $table) {
-            $table->id(); // 主キー（自動増加）
-            $table->text('code')->unique(); // 一意な課題コード（例: "I01"）
-            $table->text('issue_name'); // 課題感の名前（例: "シフトに穴が多い"）
-            $table->integer('display_order')->default(10); // 表示順序（10刻み）
-            $table->timestamps(); // 作成日時・更新日時
-        });
+        if (!Schema::hasTable('issues')) {
+            Schema::create('issues', function (Blueprint $table) {
+                $table->id();
+                $table->text('code');
+                $table->text('issue_name');
+                $table->integer('display_order')->default(10);
+                $table->timestamps();
+            });
+        }
     }
+    
 
-    /**
+    /** 
      * Reverse the migrations.
      */
     public function down(): void
