5a9df9b0ea1b9ccda76779e663d14c0e591060d9
sa st <sabstha98@gmail.com>
2025-02-12 18:09:51 +0900

    Fixed login and routes issues

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,15 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-<<<<<<< HEAD
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Http\Request; 
-=======
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
->>>>>>> 0302e1f94658b32a941265da47d40f5873256a35
 
 class LoginController extends Controller
 {
