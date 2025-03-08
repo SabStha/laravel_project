a5e47a5d7b865394754350283b4816f6f824b492
sa st <sabstha98@gmail.com>
2025-03-08 19:09:04 +0900

    c

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     
     <!-- Optionally, include your own CSS file here -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @vite(['resources/css/app/css', 'resources/js/app.js'])
     <!-- Google Font: Poppins (Cute & Bold) -->
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 
